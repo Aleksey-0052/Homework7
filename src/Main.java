988fd005ff4f954706661863f4e30d04a564d7c9
alex <alexsemen666653@gmail.com>
2023-07-09 11:14:53 +0300

    Выполнено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,107 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        }
+    public static void task1() {
+        System.out.println("Задача 1");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int monthlySavingsAmount = 15000;
+        int totalSavingsAmount = 0;
+        while (totalSavingsAmount < 2_459_000) {
+            totalSavingsAmount = totalSavingsAmount + monthlySavingsAmount;
+            int i = totalSavingsAmount / 15000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavingsAmount + " рублей");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        int a = 1;
+        while (a < 11) {
+            System.out.print(a + " ");
+            a = a + 1;
+        }
+        System.out.println();
+        for (int b = 10; b > 0; b = b - 1) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+        int year = 2023;
+        int totalPopulationOfCountry = 12000;
+        int fertility = totalPopulationOfCountry / 1000 * 17;
+        int mortality = totalPopulationOfCountry / 1000 * 8;
+        while (year < 2033) {
+            year = year + 1;
+            totalPopulationOfCountry = totalPopulationOfCountry + fertility - mortality;
+            System.out.println("Год " + year + ", численность населения составляет " + totalPopulationOfCountry);
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+        int savingsAmount = 15000;
+        int i = 0;
+        while (savingsAmount < 12_000_000) {
+            savingsAmount = savingsAmount + savingsAmount / 100 * 7;
+            i = i + 1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsAmount + " рублей");
+        }
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+        int savingsAmount = 15000;
+        int i = 0;
+        while (savingsAmount < 12_000_000) {
+            savingsAmount = savingsAmount + savingsAmount / 100 * 7;
+            i = i + 1;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsAmount + " рублей");
+            }
+        }
+        System.out.println(savingsAmount);
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+        int savingsAmount = 15000;
+        for (int i = 1; i <= 12 * 9; i++) {
+            savingsAmount = savingsAmount + savingsAmount / 100 * 7;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsAmount + " рублей");
+            }
+        }
+        System.out.println(savingsAmount);
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+        for (int dayOfWeekFriday = 3; dayOfWeekFriday <= 31; dayOfWeekFriday = dayOfWeekFriday + 7) {
+            System.out.println("Сегодня пятница, " + dayOfWeekFriday + "-е число. Необходимо подготовить отчет");
+        }
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+
+        int cometFlybyYear = 0;
+        int currentYear = cometFlybyYear + 2023;
+        int startYear = currentYear - 200;
+        int finishYear = currentYear + 100;
+        for (; cometFlybyYear < finishYear; cometFlybyYear = cometFlybyYear + 79) {
+            if (startYear < cometFlybyYear) {
+                System.out.println(cometFlybyYear);
+            }
         }
     }
 }
